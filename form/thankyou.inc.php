6a39af1b7ffe2bb35890bb89bd660549bb3cd90a
Andrea Alvarado <a.alvarado1@ufl.edu>
2021-12-12 22:54:16 -0500

    added styling to thank you form page

--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -1,3 +1,80 @@
+<style type="text/css">
+
+* {
+    margin: 0;
+    padding: 0;
+    border: 0;
+}
+
+header {
+    background: #9BD1CC;
+    z-index: 100;
+}
+
+.logo {
+    width: 40%;
+    display: inline-block;
+    text-align: center;
+    padding: 20px 0;
+}
+
+.logo:hover {
+    width: 300px;
+    transition: 2s;
+    padding: 30px 50px;
+}
+
+header nav {
+    width: 45%;
+    display: inline-block;
+    font-family: turnip, serif;
+    text-align: right;
+    vertical-align: top;
+    position: relative;
+    top: 40px;
+}
+
+header nav li {
+    display: inline-block;
+}
+
+header nav a {
+    color: #F2EFE2;
+    font-weight: bold;
+    text-decoration: none;
+    font-size: 20px;
+    margin: 0 18px;
+}
+
+header nav a:hover {
+    color:hotpink;
+
+}
+
+
+</style>
+
+<header>
+            <a class="logo" href="index.html"><img src="images/cs-logo2.png" alt="chameleon sol logo" width="300"></a>
+
+            <nav>
+                <ul>
+                    <li><a href="index.html"> Home </a></li>
+                    <li><a href="about.html"> About Us </a></li>
+                    <li><a href="contact.html"> Contact Us </a></li>
+                    <li><a href="products.html"> Products </a></li>
+                </ul>
+            </nav>
+
+</header>
+
+<div class="container">
 <h1>Thank you for your enquiry</h1>
 <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
 <p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+
+</div>
+
+<footer>
+    <p>This page is designed by Andrea Alvarado</p>
+</footer>
